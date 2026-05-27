79e8201bd27e896f232e73d2d2698dfc6f33ee43
riswan2004kolappuram-gif <riswan2004kolappuram@gmail.com>
2026-05-27 12:01:46 +0530

    added riswan

--- test/src/App.jsx
+++ test/src/App.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Riswan from './pages/Riswan'
+import Ashif from './pages/Ashif'
+import Hanshin from './pages/Hanshin'
+import Thensih from './pages/Thensih'
+import Noufa from './pages/Noufa'
+
 
 
 export default function App() {
   return (
     <div>
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Riswan />} />
+          <Route path="/thensih" element={<Thensih />} />
+          <Route path="/hanshin" element={<Hanshin />} />
+          <Route path="/noufa" element={<Noufa />} />
+          <Route path="/ashif" element={<Ashif />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
